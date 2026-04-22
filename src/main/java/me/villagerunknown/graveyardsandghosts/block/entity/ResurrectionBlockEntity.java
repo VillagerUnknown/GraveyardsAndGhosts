8e08d99710dd9ce8f1439897805dd37d35bc44df
villagerunknown <VillagerUnknown@proton.me>
2026-04-22 19:43:47 -0400

    Updating to Minecraft 1.21.4

--- src/main/java/me/villagerunknown/graveyardsandghosts/block/entity/ResurrectionBlockEntity.java
+++ src/main/java/me/villagerunknown/graveyardsandghosts/block/entity/ResurrectionBlockEntity.java
@@ -101,7 +101,7 @@ public class ResurrectionBlockEntity extends BlockEntity {
 				if( pos.isWithinDistance( player.getPos(), blockActivationDistance ) ) {
 					if(Graveyardsandghosts.CONFIG.enableParticles && Graveyardsandghosts.CONFIG.enableGraveyardBlockParticles) {
 						assert serverWorld != null;
-						serverWorld.spawnParticles(player, ParticleTypes.HEART, true, pos.getX() + 0.5, pos.getY() + 1.5, pos.getZ() + 0.5, 3, 0.25, 0.25, 0.25, 0.005F);
+						serverWorld.spawnParticles( ParticleTypes.HEART, true, true, pos.getX() + 0.5, pos.getY() + 1.5, pos.getZ() + 0.5, 3, 0.25, 0.25, 0.25, 0.005F);
 					} // if
 				} // if
 			} // if
